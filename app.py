1e84ac10fe2a6b1cf999e92e5fb7dd6a84bea6ce
aidancarr <aidand.carr925@gmail.com>
2025-02-07 23:46:32 -0500

    aidan

--- app.py
+++ app.py
@@ -6,4 +6,21 @@ st.title('Bach')
 # A simple message
 st.write('Create music with little effort . . . no AI')
 
-#
+# Mood Toggle
+st.toggle('Mood')
+
+# Tempo Drop
+# st.selectbox( label , inputs)
+st.selectbox( 'Tempo', ["Happy","Cool","Dark","Dreamy","Funky","sad","Tense"])
+
+# Jazz slider
+st.slider('Jazz')
+
+# Zane Slider
+st.slider('Zane')
+
+# Random
+st.button("Randomize")
+
+# Download
+st.button("Create!")
