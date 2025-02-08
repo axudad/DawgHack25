000fbdda9efc0716a3b331f136b3d61f156ed53d
mcs97515 <mcs97515@uga.edu>
2025-02-08 08:32:30 -0500

    YAY!

--- Michael's Backage ;D/generateChordProgression.py
+++ Michael's Backage ;D/generateChordProgression.py
@@ -1,10 +1,10 @@
 import random
 
-def generateChordProgression(length, startDegree):
+def generateChordProgression(length, startDegree, scale):
 
     chordProgression = []
 
-    startDistribution = {1 : 0.5,
+    majStartDistribution = {1 : 0.5,
                          2 : 0.65,
                          3 : 0.75,
                          4 : 0.82,
@@ -12,7 +12,7 @@ def generateChordProgression(length, startDegree):
                          6 : 0.99,
                          7 : 1}
 
-    chordTransitionProbabilities = {
+    majChordTransitionProbabilities = {
         1: {1: 0.20, 2: 0.30, 3: 0.35, 4: 0.55, 5: 0.85, 6: 0.95, 7: 1.00},
         2: {1: 0.05, 2: 0.15, 3: 0.25, 4: 0.50, 5: 0.90, 6: 0.95, 7: 1.00},
         3: {1: 0.10, 2: 0.20, 3: 0.25, 4: 0.45, 5: 0.70, 6: 0.95, 7: 1.00},
@@ -21,6 +21,33 @@ def generateChordProgression(length, startDegree):
         6: {1: 0.40, 2: 0.45, 3: 0.50, 4: 0.60, 5: 0.90, 6: 0.95, 7: 1.00},
         7: {1: 0.70, 2: 0.75, 3: 0.80, 4: 0.85, 5: 0.95, 6: 1.00, 7: 1.00}
     }
+
+    minStartDistribution = {
+        1: 0.55,
+        2: 0.56,
+        3: 0.68,
+        4: 0.76,
+        5: 0.94,
+        6: 0.98,
+        7: 1
+    }
+
+    minChordTransitionProbabilities = {
+    1: {1: 0.15, 2: 0.25, 3: 0.45, 4: 0.55, 5: 0.80, 6: 0.90, 7: 1.00},
+    2: {1: 0.20, 2: 0.30, 3: 0.45, 4: 0.70, 5: 0.80, 6: 0.95, 7: 1.00},
+    3: {1: 0.30, 2: 0.40, 3: 0.55, 4: 0.65, 5: 0.85, 6: 0.95, 7: 1.00},
+    4: {1: 0.25, 2: 0.35, 3: 0.50, 4: 0.60, 5: 0.85, 6: 0.95, 7: 1.00},
+    5: {1: 0.40, 2: 0.45, 3: 0.55, 4: 0.70, 5: 0.80, 6: 0.90, 7: 1.00},
+    6: {1: 0.20, 2: 0.35, 3: 0.45, 4: 0.60, 5: 0.80, 6: 0.90, 7: 1.00},
+    7: {1: 0.35, 2: 0.40, 3: 0.55, 4: 0.65, 5: 0.85, 6: 0.95, 7: 1.00}
+    }
+
+    if scale == "major":
+        startDistribution = majStartDistribution
+        chordTransitionProbabilities = majChordTransitionProbabilities
+    else:
+        startDistribution = minStartDistribution
+        chordTransitionProbabilities = minChordTransitionProbabilities
 
     for i in range(0, length):
         decision = random.randint(0,100) / 100.1
