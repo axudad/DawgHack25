b8261990c282668bb0b5de58532d38e759336ee2
JustinD2016 <jdorval2016@gmail.com>
2025-02-08 10:27:34 -0500

    Chords, Bass, and Leads

--- Information.py
+++ Information.py
@@ -1,18 +1,35 @@
 import random as rand
 
 class Information:
-    def __init__(self,mood, tempo, zany, jazzy):
+
+    basses = {'Synth Bass': 38,"Baratone Sax":67,"Synth Bass 2":87,"Fretless Bass":35}
+    chords = {"Drawbar Organ":17,"Electric Piano":6,"Fantasia":89,"Skakuhachi":78,"Ocarina":79,"Halo":94}
+    leads = {"Solo Vox":85,"Piano":1,"Marimba":12,"Strings":45,"Violin":40}
+
+    def __init__(self,mood, tempo, zany, jazzy, chord, bass, lead):
+        
         self.mood = mood
         self.tempo = tempo
         self.zany = zany
         self.jazzy = jazzy
+        self.chord = self.chords[chord]
+        self.bass = bass
+        self.lead = lead
 
     def __init__(self):
         pass
 
+    def get_bass(self):
+        return self.bass
+    
+    def get_chord(self):
+        return self.chord
     
     def get_jazzy(self):
         return self.jazzy
+    
+    def get_lead(self):
+        return self.lead
     
     def get_mood(self):
         return self.mood
@@ -23,8 +40,17 @@ class Information:
     def get_zany(self):
         return self.zany
     
+    def set_bass(self,bass):
+        self.bass = self.basses[bass]
+
+    def set_chord(self,chord):
+        self.chord = self.chords[chord]
+
     def set_jazzy(self,jazzy):
         self.jazzy = jazzy
+
+    def set_lead(self,lead):
+        self.lead = self.leads[lead]
 
     def set_mood(self, mood):
         self.mood = mood
@@ -38,8 +64,8 @@ class Information:
 
     def randomize(self):
         temp = ['Very Slow','Slow','Moderate','Fast','Very Fast']
-        greek = ["Happy","Cool","Dark","Dreamy","Funky","Sad","Tense"]
-        self.jazzy = rand.randint(0,100)
+        greek = ['Mixolydian','Dorian','Ionian','Lydian','Phrygian','Locrian','Aeolian']
+        self.jazz = rand.random()*100
         self.mood = greek[rand.randint(0,6)]
         self.tempo = temp[rand.randint(0,4)]
-        self.zany = rand.randint(0,100)
+        self.zane = rand.random()*100
