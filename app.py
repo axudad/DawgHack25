ce8826610e0e54c2eabe226345b28b09293c031f
aidancarr <aidand.carr925@gmail.com>
2025-02-07 22:00:55 -0500

    aidan

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from dash import Dash, html
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
 
-app = Dash()
+# Initialize the Dash app
+app = dash.Dash(__name__)
 
-app.layout = [html.Div(children='Hello World')]
+# Create the layout with a button and output text
+app.layout = html.Div([
+    html.H1("Interactive Dash Website"),
+    html.P("Click the button to change the text below."),
+    html.Button("Click Me!", id="button"),
+    html.Div(id="output-text")
+])
+
+# Set up the callback to change the output text when the button is clicked
+@app.callback(
+    Output("output-text", "children"),
+    Input("button", "n_clicks")
+)
+def update_output(n_clicks):
+    if n_clicks is None:
+        return "The button hasn't been clicked yet."
+    else:
+        return f"You clicked the button {n_clicks} times."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
