7c36c85ec4b4113ef4004ab3bde515398041730f
JustinD2016 <jdorval2016@gmail.com>
2025-02-08 11:55:39 -0500

    getters

--- Information.py
+++ Information.py
@@ -23,17 +23,26 @@ class Information:
     def __init__(self):
         pass
 
-    def get_bass(self):
-        return self.bass
+    def get_bass_int(self):
+        return self.bass_int
+
+    def get_bass_string(self):
+        return self.bass_string
     
-    def get_chord(self):
-        return self.chord
+    def get_chord_int(self):
+        return self.chord_int
+    
+    def get_chord_string(self):
+        return self.chord_string
     
     def get_jazzy(self):
         return self.jazzy
     
-    def get_lead(self):
-        return self.lead
+    def get_lead_int(self):
+        return self.lead_int
+    
+    def get_lead_string(self):
+        return self.lead_string
     
     def get_mood(self):
         return self.mood
